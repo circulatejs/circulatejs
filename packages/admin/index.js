79027abe5112ec46a78a61ad01fb349771cfba20
Greg Patrick <greg.hello@gmail.com>
2020-02-24 22:11:24 -0600

    Update index.js

--- packages/admin/index.js
+++ packages/admin/index.js
@@ -1,14 +1,23 @@
 const { exec } = require('child_process')
 
-function buildAdmin() {
-  return new Promise((resolve, reject) => {
-    exec('vue-cli-service build', (error, stdout) => {
-      if (error) {
-        reject(console.error(error))
+async function buildAdmin() {
+  return await new Promise((resolve, reject) => {
+    exec(
+      __dirname + '/node_modules/.bin/vue-cli-service build',
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(stdout, stderr)
       }
-      resolve(console.error(stdout))
-    })
+    )
   })
 }
 
 module.exports = buildAdmin()
+  .then(info => {
+    console.log(info)
+  })
+  .catch(error => {
+    console.error(error)
+  })
