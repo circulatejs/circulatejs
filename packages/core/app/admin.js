acb42bd05e72c3eec2c49ec74439999479534a63
Greg Patrick <greg.hello@gmail.com>
2020-05-21 09:12:36 -0500

    Update admin to redirect properly

--- packages/core/app/admin.js
+++ packages/core/app/admin.js
@@ -1,14 +1,26 @@
 const admin = require('@circulatejs/admin');
 
+const adminPath = process.env.ADMIN_LOCATION || '/admin'
+
 exports.plugin = {
     name: 'admin',
     register: async (server) => {
         await server.route({
             method: 'GET',
-            path: `${process.env.ADMIN_LOCATION}/{param*}`,
+            path: adminPath,
+            handler: (request, h) => {
+                // This needs to be done to make sure the base path forwards properly
+                if (request.path = `/${adminPath}`) {
+                    return h.redirect(`${adminPath}/`)
+                }
+            }
+        });
+        await server.route({
+            method: 'GET',
+            path: `${adminPath}/{param*}`,
             handler: {
                 directory: {
-                    path: './admin/',
+                    path: './admin',
                     index: ['index.html'],
                     redirectToSlash: true
                 }
