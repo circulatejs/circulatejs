7274b92485dcf1988c40e742be671c5058c08881
Greg Patrick <greg.hello@gmail.com>
2020-05-29 09:16:04 -0500

    Make sure the application loads plugins properly

--- packages/core/app/settings.js
+++ packages/core/app/settings.js
@@ -4,7 +4,8 @@ const settings = {
     HOST: process.env.HOST || 'localhost',
     PORT: process.env.PORT || 3000,
     DB_NAME: process.env.DB_NAME || 'circulatejs',
-    ADMIN_LOCATION: process.env.ADMIN_LOCATION || '/admin'
+    ADMIN_LOCATION: process.env.ADMIN_LOCATION || '/admin',
+    API_PATH: process.env.API_PATH || '/api'
 }
 
 module.exports = settings
