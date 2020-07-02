f5fe94b69c35981714f0d265a5153f53f2019b70
Greg Patrick <greg.hello@gmail.com>
2020-07-02 15:18:27 -0500

    Updates settings misspelling

--- packages/core/app/settings.js
+++ packages/core/app/settings.js
@@ -1,22 +1,20 @@
 require('dotenv').config()
 
-const proccessEnv = process.env
+const processEnv = process.env
 
-const settings = {
-  ENV: proccessEnv.ENV || 'production',
-  APP_NAME: proccessEnv.APP_NAME || 'CirculateJS',
-  HOST: proccessEnv.HOST || 'localhost',
-  PORT: proccessEnv.PORT || 3000,
-  DB: proccessEnv.DB || 'sqlite',
-  DB_HOST: proccessEnv.DB_HOST || '127.0.0.1',
-  DB_PORT: proccessEnv.DB_PORT || 3306,
-  DB_NAME: proccessEnv.DB_NAME || 'circulatejs',
-  DB_USER: proccessEnv.DB_USER || '',
-  DB_PASSWORD: proccessEnv.DB_PASSWORD || '',
-  AUTH_KEY: proccessEnv.AUTH_KEY || null,
-  ADMIN_LOCATION: proccessEnv.ADMIN_LOCATION || '/admin',
-  API_PATH: proccessEnv.API_PATH || '/api',
-  PLUGINS_PATH: proccessEnv.PLUGINS_PATH || '/plugins'
+module.exports = {
+  ENV: processEnv.ENV || 'production',
+  APP_NAME: processEnv.APP_NAME || 'CirculateJS',
+  HOST: processEnv.HOST || 'localhost',
+  PORT: processEnv.PORT || 3000,
+  DB: processEnv.DB || 'sqlite',
+  DB_HOST: processEnv.DB_HOST || '127.0.0.1',
+  DB_PORT: processEnv.DB_PORT || 3306,
+  DB_NAME: processEnv.DB_NAME || 'circulatejs',
+  DB_USER: processEnv.DB_USER || '',
+  DB_PASSWORD: processEnv.DB_PASSWORD || '',
+  AUTH_KEY: processEnv.AUTH_KEY || null,
+  ADMIN_LOCATION: processEnv.ADMIN_LOCATION || '/admin',
+  API_PATH: processEnv.API_PATH || '/api',
+  PLUGINS_PATH: processEnv.PLUGINS_PATH || '/plugins'
 }
-
-module.exports = settings
