89a79aa22315c15346862439654816206bda7af8
Greg Patrick <greg.hello@gmail.com>
2020-07-22 07:57:34 -0500

    Add rich text to UI project

--- packages/ui/lib/c-rich-text.vue
+++ packages/ui/lib/c-rich-text.vue
@@ -1,79 +1,26 @@
 <template>
-  <div class="input-container">
-    <label class="placeholder" :class="{ label: label }">{{ placeholder }}</label>
-    <input
-      ref="input"
-      @focus="setLabel"
-      @blur="unsetLabel"
-      @input="$emit('input', $event.target.value)"
-      class="input"
-      :type="type"
-    />
-  </div>
+  <div class="rich-text" ref="richText"></div>
 </template>
 
 <script>
+import Quill from 'quill'
+import '../../../node_modules/quill/dist/quill.snow.css'
+
 export default {
-  name: 'c-input',
-  props: {
-    value: String,
-    type: {
-      type: String,
-      default: 'text'
-    },
-    placeholder: {
-      type: String
-    }
-  },
+  name: 'c-rich-text',
+  props: {},
   data() {
-    return {
-      label: false
-    }
+    return {}
+  },
+  mounted() {
+    this.initQuill()
   },
   methods: {
-    setLabel() {
-      this.label = true
-    },
-    unsetLabel() {
-      if (this.$refs.input.value.length > 0) {
-      } else {
-        this.label = false
-      }
+    initQuill() {
+      return new Quill(this.$refs.richText, {
+        theme: 'snow'
+      })
     }
   }
 }
 </script>
-
-<style lang="postcss" scoped>
-.placeholder {
-  @apply px-3 py-2;
-  position: absolute;
-  color: black;
-  transition: all 0.2s ease-in-out;
-  top: 10px;
-  left: 0;
-  pointer-events: none;
-}
-.label {
-  @apply px-3 py-2 text-gray-600;
-  font-size: 80%;
-  top: -15px;
-  left: -5px;
-  pointer-events: auto;
-}
-.input-container {
-  position: relative;
-  margin-bottom: 1em;
-  padding-top: 10px;
-}
-.input {
-  border: 1px solid transparent;
-  @apply bg-gray-200 shadow w-full;
-}
-.input:focus {
-  border: 1px solid;
-  @apply border-gray-500 bg-white;
-  transition: all 0.2s ease-in-out;
-  outline: none;
-}
-</style>
