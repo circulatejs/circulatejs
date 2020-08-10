0a5d89312ab43b7a572093c8b745ca54e9276920
Greg Patrick <greg.hello@gmail.com>
2020-08-10 10:41:16 -0500

    Update admin dashboard website path

--- packages/admin/src/pages/Dashboard.vue
+++ packages/admin/src/pages/Dashboard.vue
@@ -5,11 +5,8 @@
       <p>Welcome to your new CirculateJS App!</p>
       <p>
         You can check out all you can do with this at
-        <a
-          href="https://https://circulatejs.dev/guide.html"
-          target="_blank"
-          rel="noopener noreferrer"
-          >circulate.dev</a
+        <a href="https://circulatejs.dev/guide.html" target="_blank" rel="noopener noreferrer"
+          >circulatejs.dev</a
         >
       </p>
     </div>
