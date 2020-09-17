fc0481419ec5ae8cfd436e4668e33a6457e05711
Greg Patrick <greg.hello@gmail.com>
2020-09-17 12:41:53 -0500

    Update rich text editor to accept Vue data

--- packages/ui/lib/c-rich-text.vue
+++ packages/ui/lib/c-rich-text.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="rich-text" ref="richText"></div>
+  <div class="input-container">
+    <div class="rich-text" ref="richText"></div>
+  </div>
 </template>
 
 <script>
@@ -9,17 +11,47 @@ export default {
   name: 'c-rich-text',
   props: {},
   data() {
-    return {}
+    return {
+      quill: null
+    }
   },
   mounted() {
+    const $this = this
     this.initQuill()
+    this.quill.on('text-change', () => {
+      const contents = $this.quill.getContents()
+      $this.$emit('input', JSON.stringify(contents))
+    })
   },
   methods: {
     initQuill() {
-      return new Quill(this.$refs.richText, {
+      this.quill = new Quill(this.$refs.richText, {
         theme: 'snow'
       })
+    }
+  },
+  watch: {
+    '$attrs.value': {
+      handler(val) {
+        if (val === '') {
+          const length = this.quill.getLength()
+          this.quill.deleteText(0, length)
+        }
+      },
+      deep: true
     }
   }
 }
 </script>
+
+<style lang="postcss" scoped>
+.rich-text {
+  max-height: 350px;
+  font-size: 14px;
+}
+.input-container {
+  position: relative;
+  margin-bottom: 1em;
+  padding-top: 10px;
+}
+</style>
